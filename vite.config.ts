4ed00763cc665fbe33c377de762b77826c80bde2
James Albright <James.Albright@churchofjesuschrist.org>
2025-01-10 06:40:17 -0700

    Updated vite configuration so it knows where the files will be deployed from

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base: 'https://jalbr74.github.io/power-pages-webapp/', // Set the base path to your subfolder
 })
